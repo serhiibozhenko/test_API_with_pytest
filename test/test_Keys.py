2bc9561d3eb30c6479c97593af215f371dea1138
Your Name <bozhenkoserhii@gmail.com>
2022-06-22 19:35:01 +0300

    added methods, without used class

--- test/test_Keys.py
+++ test/test_Keys.py
@@ -1,9 +1,9 @@
-api_key = {'api_key': '4422d569b364e26bc722aa8e9293d09c'}
-ses_id = {"sesion_id": 'bc37cc537ec3b27f14665e44609bd23b39af622e'}
-tok_id= {"token_id": '82448fece13b8b4f0c1997131d1f0efddd8c3a53'}
-guest_ses= {"guest_sesion": '9efc5c78c095f38bd2dbb2a394b7fe44'}
+api_key = {'api_key': ''}
+ses_id = {"sesion_id": ''}
+tok_id= {"token_id": ''}
+guest_ses= {"guest_sesion": ''}
 
 
-payload = {'api_key': '4422d569b364e26bc722aa8e9293d09c', 'guest_session_id': '9efc5c78c095f38bd2dbb2a394b7fe44', 'session_id':'bc37cc537ec3b27f14665e44609bd23b39af622e'}
+payload = {'api_key': '', 'guest_session_id': '', 'session_id':''}
 
-payloadforGET = {'api_key': '4422d569b364e26bc722aa8e9293d09c'}
+payloadforGET = {'api_key': ''}
